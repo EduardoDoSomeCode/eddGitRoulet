dce8b4d9a8fd208868894aa0980b83ce94c6bfca
eduardo.nila <nilaosornoe@gmail.com>
2024-05-30 07:03:46 -0600

    initailize repo

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const displayTextFunction = () => {
+
+    let displayText = document.querySelector(".person")
+
+    displayText.innerHTML = "Hope you have a great day , mate"
+
+}
+displayTextFunction()
